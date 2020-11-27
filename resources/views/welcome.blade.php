23654c11f0b285325494b730156308f7b225b9ef
jens210 <jensvk94@gmail.com>
2020-11-27 19:01:52 +0100

    view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -423,6 +423,7 @@
                 </svg>
             </div>
             <h1>test </h1>
+            
             <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                 <div class="grid grid-cols-1 md:grid-cols-2">
                     <div class="p-6">
